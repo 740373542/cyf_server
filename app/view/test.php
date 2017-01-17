71ca4bc1bc2a733357bc0cf0f6b0823e25bd3fee
cyf <740373542@qq.com>
2017-01-17 18:54:10 +0800

    111

--- app/view/test.php
+++ app/view/test.php
@@ -1,7 +1,23 @@
-<div class="box" style="width:100%;height:100%">
-	<div class="item" style="flex-grow:1;">CCCCCCCC</div>
-	<div class="item" style="flex-grow:10;">CCCCCCCC</div>
-	<div class="item" style="flex-grow:1;">CCCCCCCC</div>
+<div class="box_row" style="width:100%;height:10%">
+	<div class="item" style="flex-grow:2;">LOGO</div>
+	<div class="item" style="flex-grow:10;">TITLE</div>
+	<div class="item" style="flex-grow:1;">OPTION</div>
+</div>
+
+<div class="box_row" style="margin-top:10px;height:88%">
+	<div class="item" style="flex-grow:0;">MENU</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
+	<div class="item" style="flex-grow:1;margin-left:50px">CONTENT</div>
 </div>
 
 
@@ -15,11 +31,21 @@
 
 
 <style type="text/css">
-	.box{
+	.box_row{
 		display: -webkit-flex;
 		display: flex;
 		flex-wrap:wrap;
 		flex-direction:row;
+		justify-content:flex-start;
+		border: 1px solid red;
+		align-items: stretch;
+	}
+
+	.box_column{
+		display: -webkit-flex;
+		display: flex;
+		flex-wrap:wrap;
+		flex-direction:column;
 		justify-content:flex-start;
 		border: 1px solid red;
 		align-items: stretch;
